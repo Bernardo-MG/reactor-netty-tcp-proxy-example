428ea3830924b1a0968589ca5fa801478ec9f9c1
Bernardo-MG <programming@bernardo-mg.com>
2023-03-02 13:33:38 +0100

    Fixed version command

--- src/main/java/com/bernardomg/example/netty/proxy/cli/version/ManifestVersionProvider.java
+++ src/main/java/com/bernardomg/example/netty/proxy/cli/version/ManifestVersionProvider.java
@@ -38,7 +38,7 @@ public final class ManifestVersionProvider extends AbstractManifestVersionProvid
      * Default constructor.
      */
     public ManifestVersionProvider() {
-        super("Netty TCP Server Example");
+        super("Reactor Netty TCP Proxy Example");
     }
 
 }
