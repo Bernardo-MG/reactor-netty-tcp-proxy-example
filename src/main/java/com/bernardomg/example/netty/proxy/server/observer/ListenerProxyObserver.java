eefdfb913e09e2912fd4afe49ddd3e99226c56c1
Bernardo-MG <programming@bernardomg.com>
2023-03-07 08:51:34 +0100

    Added observer

---
+++ src/main/java/com/bernardomg/example/netty/proxy/server/observer/ListenerProxyObserver.java
@@ -0,0 +1,38 @@
+package com.bernardomg.example.netty.proxy.server.observer;
+
+import java.util.Objects;
+
+import com.bernardomg.example.netty.proxy.server.ProxyListener;
+
+import reactor.core.publisher.Flux;
+
+public final class ListenerProxyObserver implements ProxyObserver {
+
+    /**
+     * Proxy listener. Will received the requests.
+     */
+    private final ProxyListener listener;
+
+    /**
+     * Constructs an observer with the received listener.
+     *
+     * @param lst
+     *            proxy listener for the requests
+     */
+    public ListenerProxyObserver(final ProxyListener lst) {
+        super();
+
+        listener = Objects.requireNonNull(lst);
+    }
+
+    @Override
+    public final void setRequestFlux(final Flux<byte[]> flux) {
+        flux.doOnNext(listener::onServerReceive);
+    }
+
+    @Override
+    public final void setResponseFlux(final Flux<byte[]> flux) {
+        flux.doOnNext(listener::onClientSend);
+    }
+
+}
